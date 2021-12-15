047f518d3c6528d9614443228fb3d44e1f733848
Krzysztof Kalinowski <k.kalinowski2000@gmail.com>
2021-12-15 21:59:59 +0100

    widehardo clap

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -8,6 +8,7 @@ class SceneMain extends Phaser.Scene {
         this.load.image('wall', 'images/wall.png')
         this.load.image('coin', 'images/coin.png')
         this.load.image('home', 'images/home.png')
+        this.load.image('thief', 'images/widehardo.png')
     }
 
     create() {
@@ -22,10 +23,13 @@ class SceneMain extends Phaser.Scene {
         this.home.body.collideWorldBounds = true
         this.home.body.immovable = true
 
+        this.thief = this.physics.add.sprite(800, 800, 'thief')
+        this.thief.body.collideWorldBounds = true
+
         this.wall = this.physics.add.staticGroup()
         for (let x = 0; x < 15; x++) {
-            let brick = this.wall.create(Math.round(Math.random() * width),
-                Math.round(Math.random() * height), 'wall')
+            let brick = this.wall.create(Math.round(Math.random() * (width - 100) + 100),
+                Math.round(Math.random() * (height - 100) + 100), 'wall')
 
             brick.body.collideWorldBounds = true;
             brick.body.immovable = true
@@ -36,14 +40,23 @@ class SceneMain extends Phaser.Scene {
 
         this.cursors = this.input.keyboard.createCursorKeys()
 
-        this.coinsInBagText = this.add.text(width / 2, 0, "Coins in bag: " + this.coinsInBag, {font: "30px Arial", fill: "#000000"})
-        this.coinsInHomeText = this.add.text(0, 0, "Coins in home: " + this.coinsInHome, {font: "20px Arial", fill: "#000000"})
+        this.coinsInBagText = this.add.text(width / 2, 0, "Coins in bag: " + this.coinsInBag, {
+            font: "30px Arial",
+            fill: "#000000"
+        })
+        this.coinsInHomeText = this.add.text(0, 0, "Coins in home: " + this.coinsInHome, {
+            font: "20px Arial",
+            fill: "#000000"
+        })
     }
 
     update() {
 
         this.player.body.velocity.x = 0
         this.player.body.velocity.y = 0
+
+        this.thief.body.velocity.x = 0
+        this.thief.body.velocity.y = 0
 
         if (this.cursors.left.isDown) {
 
@@ -66,22 +79,50 @@ class SceneMain extends Phaser.Scene {
             this.player.angle = 90;
         }
 
+        if (this.thief.x > this.player.x) {
+
+            this.thief.body.velocity.x = -100
+        }
+        else if (this.thief.x < this.player.x) {
+
+            this.thief.body.velocity.x = 100
+        }
+
+
+        if (this.thief.y > this.player.y) {
+
+            this.thief.body.velocity.y = -100
+        }
+        else if (this.thief.y < this.player.y) {
+
+            this.thief.body.velocity.y = 100
+        }
+
+
         this.physics.collide(this.player, this.point, () => {
             this.coinsInBag += 1
             this.coinsInBagText.text = "Coins in bag: " + this.coinsInBag
 
-            this.point.x = Math.round(Math.random() * width)
-            this.point.y = Math.round(Math.random() * height)
+            this.point.x = Math.round(Math.random() * (width - 100) + 100)
+            this.point.y = Math.round(Math.random() * (height - 100) + 100)
         })
 
         this.physics.collide(this.player, this.wall)
 
         this.physics.collide(this.player, this.home, () => {
+
             this.coinsInHome += this.coinsInBag
             this.coinsInBag = 0
-
             this.coinsInBagText.text = "Coins in bag: " + this.coinsInBag
             this.coinsInHomeText.text = "Coins in home: " + this.coinsInHome
         })
+
+        this.physics.collide(this.player, this.thief, () => {
+
+            this.coinsInBag = 0
+            this.coinsInBagText.text = "Coins in bag: " + this.coinsInBag
+        })
+
+        this.physics.collide(this.thief, this.wall)
     }
 }
